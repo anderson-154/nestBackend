cc842a5fdc9c3a20ba357189380f06422e5d5206
Anderson <andyguaca@gmail.com>
2024-08-07 22:31:19 -0500

    create the schema user for automatic create in mongodb

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+
+    @Prop({unique: true, required:true })
+    emai:string;
+
+    @Prop({required:true})
+    password:string;
+
+    @Prop({minlength: 6, required:true})
+    name:string;
+
+    @Prop({default:true})
+    isActive:boolean;
+
+    @Prop({type:[String], default: ['user']})
+    roles:string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
